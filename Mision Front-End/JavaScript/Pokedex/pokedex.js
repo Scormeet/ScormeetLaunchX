45eddbe0291ff62e1c2bb91ba995b443ddd9c2de
Scormit <sbenitezr@outlook.com>
2022-03-21 14:38:48 -0600

    JS Checkpoint

--- Mision Front-End/JavaScript/Pokedex/pokedex.js
+++ Mision Front-End/JavaScript/Pokedex/pokedex.js
@@ -1,12 +1,30 @@
 
+
 const getPokemon = () => {
     const pokeName = document.getElementById("pokeName");
     let pokeInput = pokeName.value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/`+ pokeInput
     fetch(url).then((res) => {
-        console.log(res)
-        return res.json();
+        if(res.status != "200"){
+            console.log(res);
+            document.getElementById("pokeN").innerHTML = "Pokemon No Encontrado :(";
+            setPokeImg("./images/huh.png"); 
+        }
+        else
+            return res.json();
+    }).then((data) => {
+        document.getElementById("pokeN").innerHTML = pokeInput.toUpperCase()+"!";
+        console.log(data);
+        let pokeImg = data.sprites.front_default;
+        console.log(pokeImg);
+        setPokeImg(pokeImg);
     })
 }
 
-getPokemon();
+
+const setPokeImg = (url) => {
+    const pokeImg = document.getElementById("pokeImg")
+    pokeImg.src = url;
+}
+
+
